8a463e8052d6d877e8a2e496f820931d5ac20527
Pedro Santiago <106345894+PedroSantiagoDev@users.noreply.github.com>
2023-03-09 12:31:06 -0300

    first commit

---
+++ jobRotationRibeiraoPreto/src/Fibonacci.java
@@ -0,0 +1,30 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(calcFibonacci(55));
+        System.out.println(calcFibonacci(34));
+        System.out.println(calcFibonacci(4));
+        System.out.println(calcFibonacci(50));
+        System.out.println(calcFibonacci(1));
+    }
+
+    public static String calcFibonacci(int number) {
+        int aux = 0;
+        int indexZero = 0;
+        int indexOne = 1;
+
+        if (number == indexZero || number == indexOne) {
+            return "Pertence a sequencia Fibonacci";
+        }
+
+        while (aux < number) {
+            aux = indexZero + indexOne;
+            indexZero = indexOne;
+            indexOne = aux;
+        }
+        if (aux == number) {
+            return "Pertence a sequencia Fibonacci";
+        } else {
+            return "Não pertence a sequencia Fibonacci";
+        }
+    }
+}
---
+++ jobRotationRibeiraoPreto/src/RevertsString.java
@@ -0,0 +1,14 @@
+public class RevertsString {
+    public static void main(String[] args) {
+        System.out.println(reverts("Pedro"));
+        System.out.println(reverts("ordeP"));
+    }
+
+    public static StringBuilder reverts(String value) {
+        StringBuilder aux = new StringBuilder();
+        for (int i = (value.length() - 1); i >= 0; i--) {
+            aux.append(value.charAt(i));
+        }
+        return aux;
+    }
+}
